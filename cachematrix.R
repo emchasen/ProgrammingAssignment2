e3304502d082fdfa16ed12bb0bb7387c55998741
emchasen <emchasen@gmail.com>
2015-11-21 15:55:28 -0500

    created cachematrix.R to solve for an inverse of stored matrix

--- cachematrix.R
+++ cachematrix.R
@@ -1,32 +1,31 @@
-## Put comments here that give an overall description of what your
-## functions do
+## The below functions are used to create and store an invertable matrix, 
+## and then to call the matrix inverse
 
-## Write a short comment describing this function
+## makeCacheMatrix is a function that is used to create an invertable matrix.
+## It is really a list of four functions: 1) set() sets the value of the matrix; 
+## 2) get() calls the matrix; 3) setinv() sets the inverse of the matrix; and
+## 4) getinv() gets the value of the matrix
 
-makeCacheMatrix <- function(x = numeric()) {
+makeCacheMatrix <- function(x = matrix()) {
                 s <- NULL
+                set <- function(y) {
+                      x <<- y
+                      m <<- NULL
+                }
                 get <- function() x
-                #setinv <- function(solution) s <<- solution
-                #getinv <- function() s
-                list(get = get
-                     #setinv = setinv,
-                     #getinv = getinv
-                     )
+                setinv <- function(solution) s <<- solution
+                getinv <- function() s
+                list(set = set, get = get, 
+                     setinv = setinv, getinv = getinv)
 }
 
-jcmake <- function(y = numeric()) {
-        get <- function() y
-        set <- function(value) {
-                y <<- value
-        }
-        list(get = get, set = set)
-}
-
-## Write a short comment describing this function
+## cacheSolve calculates the inverse of the stored matrix. It first checks
+## to see if the inverse has already been calculated. If so, it returns the inverse
+## from the cache,and the message. If not, it calculates the inverse and sets the
+## inverse according to the setinv() function.
 
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
-        s <- x$getinv() #this is where my problem is
+        s <- x$getinv()
         if(!is.null(s)) {
                 message("getting cached data")
                 return(s)
